7c558a6c0c4b857b73b002c6f4e3dc705772bba4
Matthew <wonderzombie@gmail.com>
2013-12-23 11:59:00 -0800

    Add a message loop.
    Added a message loop which iterates over the incoming connection,
    invoking handlers. EOF terminates the loop. I’m trying this w/ a
    thread, just to see.
    
    Added “block-on-welcome” because why not. It consumes messages until it
    sees the welcome message. It should time out right now or check for EOF
    but it doesn’t. This may just need to be a handler like any other for
    this reason, so it can take advantage of generic reading logic.
    
    Make an irc-connection struct & use that as the generic datatype to
    pass around to functions that need to send stuff. Use the irc-message
    struct a little bit.
    
    Not all of this has been equally tested, of course. :(
    
    Next would be to stop screwing around and add a PING handler. This
    would actually allow it the bot to persist.

--- you+me+irc.rkt
+++ you+me+irc.rkt
@@ -2,71 +2,119 @@
 
 (require srfi/13)
 
-(define (make-pass-msg pass)
-  (format "PASS ~a" pass)) 
-
-(define (make-nick-msg nick)
-  (format "NICK ~a" nick))
-
-(define (make-user-msg user realname)
-  (format "USER ~a 0 * :~a" user realname))
-
-(define (make-join-msg chan)
-  (format "JOIN #~a" chan))
-
-(define (make-privmsg-msg msgtarget msg)
-  (format "PRIVMSG ~a :~a" msgtarget msg))
-
+(struct irc-connection (in out in-chan))
 (struct irc-message (prefix command params content))
 
-;; Incomplete
+;; Mutable hash for storing message handlers.
+(define message-handlers
+  (make-hash))
+
+(define (add-message-handler fn)
+  (hash-set! message-handlers (gensym) fn))
+
+(define (remove-message-handler id)
+  (hash-remove! message-handlers id))
+
+(define irc-numerics
+  (hash "001" 'RPL_WELCOME 
+        "004" 'RPL_MYINFO))
+
+(define (send-pass-msg pass out)
+  (define msg (format "PASS ~a" pass))
+  (send-msg msg out))
+
+(define (send-nick-msg nick out)
+  (define msg (format "NICK ~a" nick))
+  (send-msg msg out))
+
+(define (send-user-msg user realname out)
+  (define msg (format "USER ~a 0 * :~a" user realname))
+  (send-msg msg out))
+
+(define (send-join-msg channel out)
+  (define msg (format "JOIN #~a" channel))
+  (send-msg msg out))
+
+(define (send-privmsg-msg target content out)
+  (define msg (format "PRIVMSG ~a :~a" target content))
+  (send-msg msg out))
+
+;;;;;
+
+;; Incomplete.
+
 (define (parse-irc-message msg)
   (define parts (string-split msg))
-  (irc-message (first parts) #f #f #f))
-  
-(define init-messages
-  (list (make-pass-msg "")              ; FIXME
-        (make-nick-msg "racketbot")
-        (make-user-msg "racketbot" "racket bot")
-        (make-join-msg "test")))
+  (irc-message (first parts) (second parts) #f #f))
 
-(define (send-msg msg out)
-  (displayln msg)
+;;;;;
+
+;; Two-tiered. You have a message handler which takes a msg and a conn.
+;; The handler does something w/ message and then invokes a generic send w/ conn.
+  
+(define (send-msg msg conn)
+  (define out (irc-connection-out conn))
+  (displayln (format "-> ~a" msg))
   (fprintf out "~a\r\n" msg))
 
-(define (handle-msg msg)
+(define (handle-msg msg conn)
   (displayln msg))
 
-;; Convenience for REPL.
 (define (close-both cin cout)
   (close-input-port cin)
   (close-output-port cout))
 
 ;; For REPL.
 (define (dumb-connect)
-  (define-values (cin cout) (tcp-connect "" 6667)) ; FIXME
-  (file-stream-buffer-mode cout 'line)
-  (map (lambda (m) (send-msg m cout)) init-messages)
-  (displayln (read-line cin))
-  (values cin cout))
+  (define-values (in out) (tcp-connect "" 6667)) ; FIXME
+  (file-stream-buffer-mode out 'line)
+  (define conn (irc-connection in out #f))
+  (send-init-messages conn)
+  (block-on-welcome conn)
+  (send-join-msg "test" conn)
+  (values in out))
 
-(define (send-init-messages out)
-  (define (send msg) (send-msg m out))
-  (map send init-messages))
+(define (send-init-messages conn)
+  (send-pass-msg "" conn)
+  (send-nick-msg "racketbot" conn)
+  (send-user-msg "racketbot" "racketbot" conn))
 
+(define (block-on-welcome conn)
+  (define in (irc-connection-in conn))
+  (define welcome-code "001")           ; FIXME: un-hardcode this?
+  (let loop ()
+    (define raw-msg (read-line in))
+    (displayln (format "<- ~a" raw-msg))
+    (define msg (parse-irc-message raw-msg))
+    (define cmd (irc-message-command msg))
+    (when (or (not (string-contains-ci cmd welcome-code))
+      (loop))))
+
+(define (message-loop conn handlers)
+  (define in (irc-connection-in conn))
+  (define out (irc-connection-out conn))
+  (define (invoke-handlers line)
+    (for ([kv (hash->list handlers)])
+      ((cdr kv) line)))
+  (define (loop)
+    (define line (read-line in))
+     (cond
+      [(eof-object? line) (displayln "eof, done")]
+      [else (invoke-handlers line conn)]))
+  (loop))
+  
 (define (irc-connect server #:port [port 6667])
   (define cust (make-custodian))
   (parameterize ([current-custodian cust])
-    (define-values (cin cout) (tcp-connect server port))
-    (file-stream-buffer-mode cout 'line)
-    (send-init-messages cout)
-    (send-msg (make-join-msg "test") cout)
-    (define (loop)
-      (let ([msg (read-line cin)])
-        (when (not (equal? eof msg))
-          (displayln msg)
-          (loop))))
-    (loop)))
+    (define-values (in out) (tcp-connect server port))
+    (file-stream-buffer-mode out 'line)
+    (define conn (irc-connection in out #f))
+    (define (closer) (close-both in out))
+    (send-init-messages conn)
+    (block-on-welcome conn)
+    (send-join-msg "test" conn)
+    (thread (thunk (message-loop conn message-handlers)))
+    (values conn closer)))
 
 ;; (module+ main
 ;;   (irc-connect ""))                     ; FIXME
